7fb14bf429c86f9b23834f4c6913df909fafa6e6
kaymanthecoder <71520824+kaymanthecoder@users.noreply.github.com>
2022-10-05 00:32:23 +0200

    Add files via upload

---
+++ Machine Learning 1st project/Graph 6.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+fig = plt.figure()
+ax1 = plt.subplot(2,1,1)
+ax2 = plt.subplot(2,1,2)
+
+iris = load_iris()
+data = np.array(iris['data'])
+targets = np.array(iris['target'])
+
+cd = {0:'r',1:'b',2:"g"}
+cols = np.array([cd[target] for target in targets])
+
+ax1.scatter(data[:,3], data[:,1], c=cols)#graph 6A
+ax2.scatter(data[:,3], data[:,2], c=cols)#graph 6B
+
+plt.show()
